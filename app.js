ef71c8e00bad8627a1df1fb7b075ecd322864f70
Komov Roman <komov.r@gmail.com>
2014-12-06 22:54:20 +0400

    implemented server logic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    path = require('path');
+  path = require('path');
+
 
 /**
  * Create Express server.
@@ -12,14 +13,8 @@ var app = express();
  */
 
 var server = require('http').Server(app),
-    io = require('socket.io')(server);
+  io = require('socket.io')(server);
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
 
 /**
  * Express configuration.
@@ -30,43 +25,59 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 
 var hour = 3600000,
-    day = hour * 24,
-    week = day * 7;
+  day = hour * 24,
+  week = day * 7;
+
 app.use(express['static'](path.join(__dirname, 'public'), {
-    maxAge: week
+  maxAge: week
 }));
 
-app.get('/', function (req, res) {
-    res.render('index.html', {});
+app.get('/', function(req, res) {
+  res.render('index.html', {});
 });
 
-// Example
-// io.sockets.on('connection', function (socket) {
-//   socket.on('login', function (name) {
-//     socket.set('nickname', name, function () {
-//       console.log(name);
-//       socket.emit('ready', name);
-//       socket.broadcast.emit('ready', name);
-//     });
-//   });
+var clients = new Clients();
 
-//   socket.on('mensagem', function (msg) {
-//     socket.get('nickname', function (err, name) {
-//       socket.emit('mensagemclient', msg, name);
-//       socket.broadcast.emit('mensagemclient', msg, name);
-//     });
-//   });
+io.sockets.on('connection', function(socket) {
 
-//   socket.on('disconnect', function () {
-//     socket.get('nickname', function (err, name) {
-//       socket.broadcast.emit('mensagemclient', 'saiu', name);
-//     });
-//     console.log('saiu fora');
-//   });
-// });
+  clients.add(socket);
 
-server.listen(app.get('port'), function () {
-    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+  clients.on('add', function(user) {
+    if (clients.hasWaiting()) {
+      var game = new Game(io, clients.getWaiting());
+
+      game.on('ended'.function() {
+        this.clients.forEach(function(v) {
+          clients.setPending(this);
+        });
+      });
+    }
+
+  });
+
+  socket.on('user.info', function(data) {
+    this.nickname = data.name;
+    this.data = data;
+  });
+
+  socket.on('user.game.ready', function() {
+    //send (in room) message to start game
+    if (this.game.isTeamReady()) {
+      io.sockets.in(this.game.room).emit('game.start');
+    }
+  });
+
+  socket.on('user.game.position', function(data) {
+    io.sockets.in(this.game.room).emit('user.game.consume',data);
+  });
+
+  socket.on('user.game.consume', function(data) {
+    io.sockets.in(this.game.room).emit('user.game.consume',data);
+  });
+});
+
+server.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
 module.exports = app;
---
+++ clients.js
@@ -0,0 +1,86 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+
+function Clients() {
+
+  this.clients = [];
+
+  this.langClients = {};
+
+  this.waitingLangClients = {};
+
+  this.addNew = function(client) {
+    var self = this;
+
+    client.on('disconnect', function() {
+      self.remove(client);
+      self.emit('left', client);
+    });
+
+    this.clients.push(client);
+
+    this.emit('add', client);
+  };
+
+  this.setInfo = function(client, info) {
+
+    client.info = info;
+
+    var langHash = getLanguangeHash(client.info);
+
+    this.langClients[langHash] = this.langClients[langHash] || [];
+    this.waitingLangClients = this.waitingLangClients[langHash] || [];
+
+    this.langClients[langHash].push(client);
+    this.waitingLangClients[langHash].push(client);
+  }
+
+  this.setWaiting = function() {
+    this.waitingLangClients = this.waitingLangClients[langHash] || [];
+    this.waitingLangClients[langHash].push(client);
+  }
+
+  this.hasWaiting = function() {
+    for (var k in this.waitingLangClients)
+      if (this.waitingLangClients >= 2)
+        return true;
+
+    return false;
+  };
+
+  this.getWaiting = function() {
+    for (var k in this.waitingLangClients)
+      if (this.waitingLangClients[k] >= 2)
+        return this.waitingLangClients[k].splice(0,2);
+
+    return [];
+  }
+
+  this.remove = function(client) {
+    var info = client.info;
+
+    if (info) {
+      var langHash = getLanguangeHash(client.info);
+
+      removeFromArray(client, this.waitingLangClients[langHash]);
+      removeFromArray(client, this.langClients[langHash]);
+    }
+
+    removeFromArray(client,this.clients);
+  }
+
+  function removeFromArray(obj, array) {
+    var index = array.indexOf(client);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  }
+
+}
+
+function getLanguangeHash(info) {
+  return info.native_lang + "_" + info.learning_lang;
+}
+
+util.inherits(Clients, EventEmitter);
